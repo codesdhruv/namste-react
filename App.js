d389a46ca4cb9e96eb48335d8117deec82e7870a
codesdhruv <118097705+codesdhruv@users.noreply.github.com>
2024-05-12 12:02:23 +0530

    episode 3 laying foundation

--- App.js
+++ App.js
@@ -1,32 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// createElement => React Element-JS Object => HTMLElement(Rendered)
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" }, //attributes
+//   "Hello from react" // childrens
+// );
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" }, //attributes
-  "Hello from react" // childrens
-);
-//props = attribute + children
-console.log(heading); //at the end of day its a java script object
+const elem = <h1>React Element Using JSX</h1>;
 
-/**
- * creating a nested div(we dont use it)
- * <div id="parent">
- *  <div id="child">
- *      <h1>I am H1 div</h1>
- *      <h2>I am H2 div</h2>
- *  <div>
- * <div>
- */
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, " This is namste React"),
-    React.createElement("h2", {}, " I am H2 div"),
-  ])
+// Using JSX - not HTML, lookis like HTML
+// Always first name of functional component start with capital letter
+// At the end functional component is a normal javascript function
+const Title = () => (
+  <h1 className="heading" tabIndex="5">
+    Namaste React React Element
+    {elem}
+  </h1>
 );
-console.log(parent);
+
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="parent"> Namaste React Function Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
