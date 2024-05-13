ed41e96dfd9448c3102064610b3986203fd60100
codesdhruv <118097705+codesdhruv@users.noreply.github.com>
2024-05-13 15:30:37 +0530

    episode-05 part1 folder restructure

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ JSX => createElement => React Element-JS Object => HTMLElement(Rendered)
 // Using JSX - is not HTML, looks like HTML
 // Always first name of functional component start with capital letter
 // At the end functional component is a normal javascript function
+
+Default export/import
+export default component
+import component from "PATH"
+
+Named export/import
+export const component
+import {component} from "PATH"
---
+++ src/components/Body.js
@@ -0,0 +1,16 @@
+import restList from "../../utils/mockData";
+import ReataurantCard from "./ReataurantCard";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"> search</div>
+      <div className="res-container">
+        {restList.map((restaurant) => (
+          <ReataurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Body;
