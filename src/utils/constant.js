339d7977d79833ad19fc4d4c0b61c1e1431f9a6d
codesdhruv <118097705+codesdhruv@users.noreply.github.com>
2024-05-13 15:50:07 +0530

    episode 5 part1

---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://dynamic.brandcrowd.com/asset/logo/0750841d-0ffd-4757-9229-20bc31001784/logo-search-grid-1x?logoTemplateVersion=1&v=638321759246830000";
