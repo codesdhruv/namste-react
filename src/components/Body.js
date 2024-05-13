56139e0af65c762049e8c0e11af3fe13cb9110ec
codesdhruv <118097705+codesdhruv@users.noreply.github.com>
2024-05-13 17:42:30 +0530

    episode-05 part2 let gets hooked

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,27 @@
 import restList from "../utils/mockData";
 import ReataurantCard from "./ReataurantCard";
+import React, { useState } from "react";
 
 const Body = () => {
+  // state variable -> super powerful variable
+  const [listofRestaurants, setListofRestarants] = useState(restList);
   return (
     <div className="body">
-      <div className="search"> search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredlist = listofRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListofRestarants(filteredlist);
+          }}
+        >
+          Top rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {restList.map((restaurant) => (
+        {listofRestaurants.map((restaurant) => (
           <ReataurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
