5a13c7273895a380037e405e3ea3eb6d6235bb66
codesdhruv <118097705+codesdhruv@users.noreply.github.com>
2024-05-17 15:51:01 +0530

    ep 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,81 @@
-import restList from "../utils/mockData";
 import ReataurantCard from "./ReataurantCard";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   // state variable -> super powerful variable
-  const [listofRestaurants, setListofRestarants] = useState(restList);
-  return (
+  const [listOfRestaurants, setListOfRestraunt] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  // Whenever state variables update, react triggers a reconciliation cycle(re-renders the component)
+  console.log("Body Rendered");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.675276800000006&lng=77.1588096&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(
+      json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    //optional chaining
+    setListOfRestraunt(
+      json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  // if (listofRestaurants.length == 0) {
+  //   return <Shimmer />;
+  // }
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+
+          <button
+            onClick={() => {
+              //Filter the restraunt and update the UI
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredlist = listofRestaurants.filter(
-              (res) => res.data.avgRating > 4
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
             );
-            setListofRestarants(filteredlist);
+            setFilteredRestaurant(filteredList);
           }}
         >
           Top rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listofRestaurants.map((restaurant) => (
-          <ReataurantCard key={restaurant.data.id} resData={restaurant} />
+        {filteredRestaurant.map((restaurant) => (
+          <ReataurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import { LOGO_URL } from "../../utils/constant";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnNameReact, setbtnNameReact] = useState("login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,16 @@ const Header = () => {
           <li>About us</li>
           <li>Contact</li>
           <li>cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "login"
+                ? setbtnNameReact("logout")
+                : setbtnNameReact("login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
