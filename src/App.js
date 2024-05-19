4808cc62368844a6b0de4ff9600edb5b130a4d8d
codesdhruv <118097705+codesdhruv@users.noreply.github.com>
2024-05-19 12:06:22 +0530

    episode 8

--- src/App.js
+++ src/App.js
@@ -2,15 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/About";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppContainer = () => {
   return (
     <div id="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
-
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppContainer />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppContainer />);
+root.render(<RouterProvider router={appRouter} />);
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,3 +3,6 @@ export const CDN_URL =
 
 export const LOGO_URL =
   "https://dynamic.brandcrowd.com/asset/logo/0750841d-0ffd-4757-9229-20bc31001784/logo-search-grid-1x?logoTemplateVersion=1&v=638321759246830000";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8466937&lng=80.94616599999999&restaurantId=";
